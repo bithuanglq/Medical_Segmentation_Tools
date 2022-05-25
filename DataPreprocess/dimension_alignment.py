a43d308b38752f40be0213515d030abd289c2331
bithuanglq <46642752+bithuanglq@users.noreply.github.com>
2022-05-25 11:59:42 +0800

    Update dimension_alignment.py

--- DataPreprocess/dimension_alignment.py
+++ DataPreprocess/dimension_alignment.py
@@ -1,5 +1,9 @@
-import SimpleITK as sitk
+import os
 import numpy as np
+import SimpleITK as sitk
+
+
+
 
 
 
@@ -72,3 +76,60 @@ def resize_with_cropping(datapath, dirpath, new_shape):
     padded_data = sitk.GetImageFromArray(array)
     padded_data.SetSpacing(spacing)
     sitk.WriteImage(padded_data, dirpath)
+    
+    
+    
+    
+    
+   
+
+def padding_and_cropping(target_patchsize:np.array, datapath:str, dirpath:str)
+    '''
+        resize resampled  data, the same size as the input of NN
+        datapath: e.g. data/1.nii.gz
+    '''
+
+    # If the patch size is not divisible by 2^n(d) for each axis, 
+    # where n(d) is the number of downsampling operations, it is padded accordingly.
+    mod = 2**4
+
+    for i in range(len(target_patchsize)):
+        slices = target_patchsize[i]
+        if slices%mod > mod//2:
+            target_patchsize[i] = (slices//mod+1)*mod
+        else:
+            target_patchsize[i] = (slices//mod)*mod
+    print(target_patchsize)                             #  (z,y,x)  e.g. (96, 576, 576)
+
+
+    data = sitk.ReadImage(os.path.join(datapath))
+    array = sitk.GetArrayFromImage(data)
+    spacing = data.GetSpacing()
+    patchsize = array.shape
+    patchsize = np.array(patchsize)
+
+    # alignment of patchsize, padding before cropping
+    delta = target_patchsize - patchsize
+    delta = delta.astype('int64')
+    lb,ub = delta//2, (delta//2)+(delta%2)
+
+    array = np.pad(array, (
+                (max(0, lb[0]), max(0, ub[0])),
+                (max(0, lb[1]), max(0, ub[1])),
+                (max(0, lb[2]), max(0, ub[2]))
+        ), mode='edge')                             # padding mode
+
+    array = array[
+        max(0, -lb[0]) : (array.shape[0] - max(0, -ub[0])),
+        max(0, -lb[1]) : (array.shape[1] - max(0, -ub[1])),
+        max(0, -lb[2]) : (array.shape[2] - max(0, -ub[2]))
+    ]
+
+
+
+
+    # save
+    print(patchsize, array.shape)
+    padded_data = sitk.GetImageFromArray(array)
+    padded_data.SetSpacing(spacing)
+    sitk.WriteImage(padded_data, os.path.join(dirpath))
