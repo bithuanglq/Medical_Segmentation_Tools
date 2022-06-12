d663f89c21a23ec524207dd6e74ce402d0a54f26
bithuanglq <46642752+bithuanglq@users.noreply.github.com>
2022-06-12 16:15:41 +0800

    Update dice.py

--- Metirc/dice.py
+++ Metirc/dice.py
@@ -4,8 +4,8 @@ import torch
 
 
 def dice_loss_oneclass(input:np.array,target:np.array):
-    # input:(N,256,256) numpy
-    # target:(N,256,256) numpy
+    # input:(N,x,y,z) numpy
+    # target:(N,x,y,z) numpy
     inputs = input.flatten()
     targets = target.flatten()
     intersection = inputs*targets
@@ -16,10 +16,10 @@ def dice_loss_oneclass(input:np.array,target:np.array):
 
 
 def dice_coef_loss(input:torch.Tensor,target:torch.Tensor,alpha=[1,0]):   # len(alpha) == nclass
-    # input: (N,nclass,256,256) torch 
-    # target:(N,nclass,256,256) torch
+    # input: (N,nclass,x,y,z) torch 
+    # target:(N,nclass,x,y,z) torch
     nclass = target.shape[1]
     alpha = torch.Tensor(alpha)
-    dice_loss = torch.sum(torch.Tensor([alpha[i]*dice_loss_oneclass(input[:,i,:,:],target[:,i,:,:]) for i in range(nclass)]))
+    dice_loss = torch.sum(torch.Tensor([alpha[i]*dice_loss_oneclass(input[:,i,:,:,:],target[:,i,:,:,:]) for i in range(nclass)]))
     dice_loss = dice_loss/torch.sum(alpha)
     return dice_loss
